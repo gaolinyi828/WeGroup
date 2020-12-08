fb19d3b39f5beb64d9b5c4d7c5be3c8de3d4b464
libertyliu <liu.siyu4@husky.neu.edu>
2020-12-08 00:33:50 -0800

    Add files via upload

--- backend/routes/team.js
+++ backend/routes/team.js
@@ -1,7 +1,20 @@
-const express = require("express");
-const router = express.Router({mergeParams: true});
+const mongoose = require("mongoose");
 
-// TODO
-// router.put etc.
+const TeamSchema = new mongoose.Schema({
+    // TO-DO: add team schema properties
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"},
+    members: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+    teamName: String,
+    tag: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tag"
+    },
+    postId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post"
+    }
+});
 
-module.export = router;
+module.exports = mongoose.model("Team", TeamSchema);
