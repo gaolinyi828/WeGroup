4aeac0a31db4d485d155241643880e20335adc56
libertyliu <liu.siyu4@husky.neu.edu>
2020-12-08 00:34:28 -0800

    Add files via upload

--- backend/routes/team.js
+++ backend/routes/team.js
@@ -1,20 +1,87 @@
-const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router({mergeParams: true});
+const Post = require('../models/post');
+const User = require('../models/user');
+const Team = require('../models/team');
 
-const TeamSchema = new mongoose.Schema({
-    // TO-DO: add team schema properties
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"},
-    members: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
-    teamName: String,
-    tag: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Tag"
-    },
-    postId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Post"
+/**
+ * Create a team
+ *
+ */
+router.post(
+    '/post/post_id/team', async (req, res) => {
+        try {
+            const newTeam = new Team({
+                userId: req.body.userId,
+                members: req.body.interested,
+                teamName: req.body.text,
+                postId: req.post.id,
+                tag: req.body.tag
+            });
+
+            const team = await newTeam.save();
+
+            res.json(team);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
+
+/**
+ * Delete a team with teamId
+ * @param id teamId
+ *
+ */
+router.delete('/team/delete/:teamId', async (req, res) => {
+    try {
+        const team = await Team.findById(req.params.id);
+
+        if (!team) {
+            return res.status(404).json({ msg: 'Team not found' });
+        }
+        await Team.remove();
+
+        res.json({ msg: 'Team removed' });
+    } catch (err) {
+        console.error(err.message);
+
+        res.status(500).send('Server Error');
     }
 });
 
-module.exports = mongoose.model("Team", TeamSchema);
+/**
+ * Get all InterestedUserList by postId
+ * @param id userId
+ */
+router.get('/team/getInterestedUserList/:postId', async ({params: {postId}}, res) => {
+    try {
+        const userList = await Post.find({post: postId}).populate('interested',['userName']);
+        res.json(userList);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+/**
+ * Get a Team list by userID
+ * @param userID
+ */
+router.get('/team/getTeamList/:userId', async (req, res) => {
+    try {
+        const teams = await User.findById(req.params.id).populate('teams', ['teamName']);
+
+        if (!teams) {
+            return res.status(404).json({ msg: 'Team not found' })
+        }
+
+        return res.json(teams);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server Error');
+    }
+});
+
+module.export = router;
