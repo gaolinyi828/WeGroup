1207caf7a485dc3745f68b42d512f31760b471c0
hongdanzhu <zhu.ho@husky.neu.edu>
2020-12-10 03:55:14 -0800

    Add files via upload

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -11,61 +11,26 @@ const Post = require("../models/post");
 const Team = require("../models/team");
 const User = require("../models/user");
 
-// createUser
 // updateUser (update different fields can have different functions)
 // getUser
-
-/**
- * Create a new user and save to database
- *
- * @param username
- * @param password
- * @param avatar
- *
- * @return status 400 if input is invalid
- *         status 201 with new user object
- */
-
-router.post('/users/signup', (req, res) => {
-    const user = new User({
-        userName: req.body.userName,
-        userPassword: req.body.userPassword,
-        avatar: req.body.avatar
-    });
-    user.save((err, user) => {
-        if (err) {
-            console.log(err)
-            res.status(400).send("invalid input");
-        } else {
-            const payload = {
-                user: {
-                    id: user.id
-                }
-            };
-            jwt.sign(payload, secret, { expiresIn: '1h'}, (err, token) => {
-                if (err) throw err;
-                res.json({ token, user });
-            });
-        }
-    })
-});
-
 
 /**
  * Update a user with the userId and new information
  *
  * @param userId the user id
- * @param username
+ * @param userName the user's name
+ * @param userPassword the user's password
+ * @param avatar the user's avatar
  * @return status 404 if not found
  *         status 200 with user object
  */
 // update a user
 router.put('/users/update', async (req, res) => {
-    User.findByIdAndUpdate(req.params.userId, req.body.userName, (err, userName, userPassword, avatar) => {
+    User.findByIdAndUpdate(req.params.userId, {userName: req.body.userName, userPassword: req.body.userPassword, avatar: req.body.avatar}, (err, user) => {
         if (err) {
             res.status(404).send("Something went wrong");
         } else {
-            res.status(200).send("update complete");
+            res.status(200).send(user);
         }
     });
 })
@@ -74,7 +39,7 @@ router.put('/users/update', async (req, res) => {
 /**
  * Get the user information by user id
  *
- * @param  userId the ser id to search
+ * @param  userId the user id to search
  * @return status 404 if not found
  *         status 200 with user object
  */
@@ -90,4 +55,4 @@ router.get('/users/:id', (req, res) => {
 });
 
 
-module.exports = router;
+module.export = router;
