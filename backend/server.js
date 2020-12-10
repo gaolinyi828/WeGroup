d4ac1319d695f770d7792c5187c151aadd44f5b1
hongdanzhu <zhu.ho@husky.neu.edu>
2020-12-10 04:12:26 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,40 @@
+// const express = require("express");
+// const app = express();
+// const mongoose = require("mongoose");
+// const port = process.env.PORT || 3000;
+
+// app.use(express.json());
+// app.use('/', require('./routes/post'));
+
+// const uri = "mongodb+srv://new_user:55001234@cluster0.tpbel.mongodb.net/WeGroup?retryWrites=true&w=majority";
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// app.listen(port, () => {
+//     console.log("WeGroup server starts listening at port " + port);
+// });
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
+const indexRoutes = require("./routes/index");
+const tagRoutes = require("./routes/tag");
+const teamRoutes = require("./routes/team");
+const commentRoutes = require("./routes/comment");
+const userRoutes = require("./routes/user");
 
 app.use(express.json());
-app.use('/', require('./routes/post'));
 
 const uri = "mongodb+srv://new_user:55001234@cluster0.tpbel.mongodb.net/WeGroup?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use('/', indexRoutes);
+app.use('/tags', tagRoutes);
+app.use('/teams', teamRoutes);
+app.use('/users', userRoutes);
+app.use('/post/:id/comment', commentRoutes);
+app.use('/', require('./routes/post'));
+app.use('/', require('./routes/user'));
 
 app.listen(port, () => {
     console.log("WeGroup server starts listening at port " + port);
