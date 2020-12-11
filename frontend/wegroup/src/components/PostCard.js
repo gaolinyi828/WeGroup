cf216a35df9ee08e1ba16c2337d94d799b6a1878
Yongle Lin <linyongle@Yongles-MacBook-Pro.local>
2020-12-10 16:19:26 -0800

    Activity styling change

--- frontend/wegroup/src/components/PostCard.js
+++ frontend/wegroup/src/components/PostCard.js
@@ -5,7 +5,7 @@ class PostCard extends Component {
 //param postid, render a card showing post name, who posted it(show profile image), user name, posted date, and some comments, set a max length of xxx
     render() {
         return (
-            <Card style={{ width: '38rem' }}>
+            <Card style={{ width: '48rem' }}>
                 <Card.Body>
                     <Card.Title>{this.props.post.title}</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">Post Subtitle</Card.Subtitle>
