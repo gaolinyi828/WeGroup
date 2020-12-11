1a9c1440c87963216a76b539a72773470dbad623
Qing Liao <lq@Qings-MacBook-Pro.local>
2020-12-10 19:48:31 -0800

    connect create post with backend

---
+++ frontend/wegroup/src/services/TagService.js
@@ -0,0 +1,47 @@
+let _singleton = Symbol();
+const TAG_API_URL = 'http://localhost:8080/tags';
+
+
+class TagService {
+    constructor(singletonToken) {
+        if (_singleton !== singletonToken)
+            throw new Error('Cannot instantiate directly.');
+    }
+
+    static get instance() {
+        if (!this[_singleton])
+            this[_singleton] = new TagService(_singleton);
+        return this[_singleton]
+    }
+
+    createTag(tag) {
+        return fetch(`${TAG_API_URL}/create`, {
+            body: JSON.stringify(tag),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST'
+            });
+    }
+
+    getAllTags() {
+        return fetch(`${TAG_API_URL}/`, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'GET'
+        });
+    }
+
+    searchTagByKeys(tagKeys) {
+        return fetch(`${TAG_API_URL}/search`, {
+            body: JSON.stringify(tagKeys),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'GET'
+        })
+    }
+}
+
+export default TagService;
